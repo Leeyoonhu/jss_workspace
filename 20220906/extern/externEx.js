fb1470b7d8c2cc6685acd76b3b583a8ab9c4dc99
Leeyoonhu <316902@naver.com>
2022-09-06 17:09:31 +0900

    first commit

---
+++ 20220906/extern/externEx.js
@@ -0,0 +1,8 @@
+var age = 10;
+document.write(
+  "<div style = 'color: red; font-size:24px;'> 외부 자바 스크립트 파일</div>"
+);
+document.write("당신의 나이는 " + age + "세 입니다.");
+
+// 외부에서 작성할땐 script tag 필요없음
+// 이 js 를 불러올 html 예시파일 생성
