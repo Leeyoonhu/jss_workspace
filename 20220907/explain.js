65391ffd80f85855af83971f16f6195ca5d5c5a6
Leeyoonhu <316902@naver.com>
2022-09-07 09:50:42 +0900

    commit~

---
+++ 20220907/explain.js
@@ -0,0 +1,21 @@
+/*
+    내장 객체의 종류(p352 참조)
+    BOM(Browser Object Model) : 브라우저 객체 모델
+    DOM(Document Object Model) : 문서 객체 모델(body line)
+    GJO(Global JavaScript Objects) : 전역 자바스크립트 객체
+
+    <h1> 요소 </h1> ==> 요소를 innerHTML 속성이라 하며, script를 이용해 이런 HTML속성을 바꿀예정임
+
+    DOM Tree(p354 참조)
+    html이 시작될 때, 각 태그들을 객체화
+    tree 구조는 가장 상단을 root
+    부모, 자식, 형제 개념이 있음
+
+    기존의 HTML 태그 생성은 정적 생성이였으나,
+    script를 통해 동적으로 생성해줌
+
+    문서 객체 생성(create DOM)
+    createElement() => 요소 노드 생성
+    createTextNode() => 텍스트 노드 생성
+    appendChild() => 요소 노드 body객체에 추가
+*/
