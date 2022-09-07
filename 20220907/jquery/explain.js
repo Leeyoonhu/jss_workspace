25622f60b43d7b1ed5de95f8f5de4120241a34f8
Leeyoonhu <316902@naver.com>
2022-09-07 14:59:44 +0900

    jQuery

---
+++ 20220907/jquery/explain.js
@@ -0,0 +1,27 @@
+/*
+    제이쿼리(jQuery) 함수
+    - 기본형 jQuery(선택자).메소드
+    - $(선택자).메소드
+
+    사용 예)
+    $("p").hide() 
+
+    문서 준비 이벤트 코드(== window.onload)
+    - $(document).ready(function(){
+        
+        // jQuery methods
+
+    })
+
+    마우스 이벤트
+    - mouseenter : 선택자 안에있으면 호출
+    - mouseleave : 선택자 밖에있으면 호출
+    - mousedown : 마우스가 눌린 상태면 호출
+    - mouseup : 마우스 버튼에서 손을 떼면 호출
+
+    jQuery로 css 컨트롤하기
+    1. <head> 태그의 <style>태그에 특정조건에서 행할 css가 담긴 클래스 작성 
+    addClass : jQuery로 준 조건에서 태그들에 css가 담긴 class를 더하기
+    removeClass : jQuery로 준 조건에서 태그들에 css가 담긴 class를 빼기
+    toggleClass : jQuery로 준 조건 실행될때마다 add, remove를 번갈아가면서 실행
+*/
