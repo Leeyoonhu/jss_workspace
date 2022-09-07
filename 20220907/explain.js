3f3c9e4772764f53a821e59333ab419619250a6f
Leeyoonhu <316902@naver.com>
2022-09-07 10:42:33 +0900

    commit!

--- 20220907/explain.js
+++ 20220907/explain.js
@@ -14,8 +14,23 @@
     기존의 HTML 태그 생성은 정적 생성이였으나,
     script를 통해 동적으로 생성해줌
 
+
     문서 객체 생성(create DOM)
     createElement() => 요소 노드 생성
     createTextNode() => 텍스트 노드 생성
     appendChild() => 요소 노드 body객체에 추가
+
+
+    문서 객체 선택(p367 참조)
+    - 1개만 선택
+    document.getElementById(아이디) => 아이디로 1개 선택
+    documnet.getSelector(선택자) => 문서상 가장 상단의 선택자 1개만 가져옴
+    - 여러개 선택
+    document.getElementsByName(name) => tag의 name 속성이 ElementsByname(name)과 일치할 경우, 배열로 가져옴
+    document.getElementsByTagName(tagName) => tagName과 일치하는 애들 가져옴
+
+
+    글자 조작
+    - innerHTML : HTML tag 적용됨
+    - textContent  : HTML tag 적용안됨 (<h1>..이런태그들도 함께 출력)
 */
