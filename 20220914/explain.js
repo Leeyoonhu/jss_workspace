82f162b4bf8cfdf444d492eedea9e499008ff0e0
Leeyoonhu <316902@naver.com>
2022-09-14 09:52:48 +0900

    20220914-jQuery-Ajax01

---
+++ 20220914/explain.js
@@ -0,0 +1,15 @@
+/*
+    $.ajax() methods
+    - success(data, xhr, status) : ajxa성공 이벤트 리스너 지정
+    - error(xhr, status, erorr) : ajax실패 이벤트 리스너 지정
+    - complete(xhr, status) : ajax완료 이벤트 리스너 지정
+    - type : get, post 방식지정
+    
+    jQuery Ajax methods
+    - $.get() : get 방식으로 ajax수행
+    - $.post() : post 방식으로 ajax수행
+    - $.(selector).load() : ajax를 수행하고 선택자로 선택한 문서 객체 안에 적재시킴
+    - $.getJSON() : method = "get"으로 ajax수행해 JSON 데이터 가져옴
+    - $.getScript() : method = "get"으로 ajax수행해 Script 데이터 가져옴
+
+*/
